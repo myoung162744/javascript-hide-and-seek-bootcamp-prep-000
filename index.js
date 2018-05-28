4d44a4e5601a5838636203bcbb88647b4d7460d2
Matthew Young <myoung162744@gmail.com>
2018-05-28 16:40:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ function increaseRankBy(n) {
 function deepestChild() {
   var e = document.getElementById('grand-node').querySelectorAll('div')
   for (let i = 0; i < e.length; i++) {
-    
+    var d = e[i]
   }
+  return d
 }
