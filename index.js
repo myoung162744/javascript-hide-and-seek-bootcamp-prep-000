dad6dce16c19048c8ca0d83e2c03b98f4c187e42
Matthew Young <myoung162744@gmail.com>
2018-05-28 16:22:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,6 +12,6 @@ function increaseRankBy(n) {
   var lis = document.getElementById('app').querySelectorAll('ul.ranked-list li')
   for (let i = 0; i < lis.length; i++) {
     var calc = parseInt(lis[i].innerHTML)
-    calc + n
+    lis[i].innerHTML = calc + n
   }
 }
