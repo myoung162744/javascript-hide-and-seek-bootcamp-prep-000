8c37cc74267495963482a805db2195fe9867bd92
Matthew Young <myoung162744@gmail.com>
2018-05-28 16:15:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 function getFirstSelector(selector) {
-  var n = document.querySelector(selector)
-  return n
+  var s = document.querySelector(selector)
+  return s
 }
 
 function nestedTarget() {
   var a = document.querySelector('#nested').querySelector('.target')
   return a
+}
+
+function increaseRankBy(n) {
+  var lis = document.getElementById('app').querySelectorAll('ul.ranked-list li')
+  for (let i = 0; i < lis.length; i++) {
+    var calc = parseInt(lis[i].innerHTML) + n
+  }
 }
